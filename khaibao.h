41c1061aee96b2f38681cf756606ed4dc24f29d8
LeAnh <leanhxtech@gmail.com>
2021-07-26 20:01:08 +0700

    Update khaibao.h

--- khaibao.h
+++ khaibao.h
@@ -1,19 +1,27 @@
 #include<string.h>
 
+const int MACDINH=0;
+const int MD_TTCB=1;
+const int MAX_MB=300;
+const int MAX_VE=200;
+
 int top1[10],left1[10],right1[10],bottom1[10],top2[10],left2[10],right2[10],bottom2[10],
 t_scb[10],l_scb[10],r_scb[10],b_scb[10],t_hcb[10],l_hcb[10],r_hcb[10],b_hcb[10],
-t_dcb[10],l_dcb[10],r_dcb[10],b_dcb[10],t_dsv[10],l_dsv[10],r_dsv[10],b_dsv[10];
-
+t_dv[10],l_dv[10],r_dv[10],b_dv[10],t_dsv[10],l_dsv[10],r_dsv[10],b_dsv[10],
+t_ghe[200],l_ghe[200],r_ghe[200],b_ghe[200],tt_ghe[200];
+//tt_ghe: 0 la chua dat, 1 la da dat, 2 la da huy
 
 struct maybay{
 	char sohieumb[16];
 	char loaimaybay[41];
 	int socho;
+	int sl_bay=MACDINH;
+	bool tt=false;
 };
 typedef struct maybay MB;
 
 struct danhsachmaybay{
-	MB *data[301];
+	MB *data[MAX_MB];
 	int sl_mb;
 };
 typedef struct danhsachmaybay DSMB;
@@ -30,33 +38,40 @@ typedef struct thoi_gian TIME;
 struct khach_hang{
 	char CMND[10];
 	char Ho[10];
-	char Ten[20];
+	char Ten[30];
 	bool Phai; //true:nam-false:nu
 };
 typedef struct khach_hang KH;
 
+struct nodeKH{
+	KH data;
+	int n = MACDINH;
+	struct nodeKH *left = NULL;
+	struct nodeKH *right = NULL;
+};
+typedef nodeKH *TREE;
+
 struct ve{
 	char CMND[10];
-	int ve;
+	int ghe_so;
+	bool tt=true;// true la co nguoi dat, false la ve bi huy
 };
 typedef struct ve VE;
 
 struct chuyen_bay{
 	char machuyenbay[16];
 	TIME time_xp;
-	char tg[30];
 	char noiden[16];
-	int trang_thai=1;
+	int trang_thai= MD_TTCB;
 	char sh_mb[16];
-	//int slve;
-	int stt;
-	//VE *dsve[100];
 };
 typedef struct chuyen_bay CB;
 
 struct nodeCB{
 	CB data;
-	VE *dsve[100];
+	int slve;
+	int sldat;
+	VE *dsve[MAX_VE];
 	struct nodeCB* next = NULL;
 };
 typedef struct nodeCB* PTR;
@@ -67,13 +82,16 @@ const int f_ds = 3;// kc là 1
 
 const int ID = 1;
 const int So = 2;
-const int HoTen =3;
+const int Ten = 3;
+const int CMVE = 4;
+const int CMND = 5;
+const int Ho = 6;
 
 const int so_box = 3;
 const int so_box1 = 3;
 const int so_dsmb = 4;
 const int so_cn = 5;
-const int max_day[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+
 
 
 char ten[20] =   "QUAN LY SAN BAY";
